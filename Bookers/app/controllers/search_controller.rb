8e20c2e84014da4265739b178e104559eac3d89d
花田　結衣 <yui@hanadayuinoMBP.lan>
2020-05-22 18:10:56 +0900

    [finish]sub5

--- Bookers/app/controllers/search_controller.rb
+++ Bookers/app/controllers/search_controller.rb
@@ -1,14 +1,38 @@
 class SearchController < ApplicationController
 	def search
-		@user_or_book = params[:option]
-		if @user_or_book == "user"
-			@users = User.where('name LIKE ?', "%#{params[:search]}%")
+		@search = params[:search]
+		@options = params[:option]
+		@ways = params[:way]
+
+		if @options == "user"
+			if @ways == "perfect"
+				@users = User.where(name: "#{@search}")
+			end
+			if @ways == "front"
+				@users = User.where('name LIKE ?', "#{@search}%")
+			end
+			if @ways == "behind"
+				@users = User.where('name LIKE ?', "%#{@search}")
+			end
+			if @ways == "part"
+				@users = User.where('name LIKE ?', "%#{@search}%")
+			end
 		end
-		if @user_or_book == "book"
-			@books = Book.where('body LIKE ?', "%#{params[:search]}%")
+
+		if @options == "book"
+			if @ways == "perfect"
+				@books = Book.where(body: "#{@search}")
+			end
+			if @ways == "front"
+				@books = Book.where('body LIKE ?', "#{@search}%")
+			end
+			if @ways == "behind"
+				@books = Book.where('body LIKE ?', "%#{@search}")
+			end
+			if @ways == "part"
+				@books = Book.where('body LIKE ?', "%#{@search}%")
+			end
 		end
 	end
 
-	def perfect_search
-	end
 end
